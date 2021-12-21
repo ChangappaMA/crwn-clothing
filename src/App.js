dd06d6a909f419ce06128f407b27d642e07db6a6
Changappa_m_a <Changappa_m_a@dell.com>
2021-12-21 12:42:11 +0530

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route path='/' exact component={HomePage} />
-        <Route path='/shop' exact component={ShopPage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/shop" exact component={ShopPage} />
       </Switch>
     </div>
   );
