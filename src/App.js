3931511498afe16b5ee2b4e25fb475b799fcb1d6
Changappa_m_a <Changappa_m_a@dell.com>
2021-12-27 13:43:26 +0530

    added cartIcon and Dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import { connect } from "react-redux";
 
@@ -40,17 +40,31 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route path="/" exact component={HomePage} />
+          <Route path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
